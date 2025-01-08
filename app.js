666738579412e00c9511fdf19e2563f2129232eb
CHJ <huihee@g.hongik.ac.kr>
2025-01-08 12:13:11 +0900

    순수 자바스크립트의 동적 타입 지정 예시

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 console.log("hello")
+
+let a = 10; //숫자, number
+let b = "Hello"; // 문자열, string
+let c = true; // false, Boolean 참, 거짓
+let d = [1,2,3,4]; // Array, 배열 (배열도 Object)
+let e = { name : "Tom", age : 35 }; // Object 객체
+//..
+
+
+b = 50;
+
+
+//..
+console.log("a=" + a, ", type of " + typeof a)
+console.log("b=" + b, ", type of " + typeof b)
+console.log("c=" + c, ", type of " + typeof c)
+console.log("d=" + d[0], ", type of " + typeof d)
+console.log("e.name=" + e.name, "e.age=" + e.age, ", type of " + typeof e)
+
+// 예시
+
+let columnLength = 10; // 길이를 cm 단위로 number로 지정
+//.. 개발진행과정
+//.. 개발진행과정
+//.. 개발진행과정
+//.. 개발진행과정
+//.. 개발진행과정
+columnLength = "30cm";
+
+//.. 개발진행과정
+//.. 개발진행과정
+//.. 개발진행과정
+let addLength = columnLength + 5;
+let meterLength = addLength / 10;
+
+//.. 개발진행과정
+//.. 개발진행과정
+//.. 개발완료
+
+console.log(addLength)
+console.log(meterLength)
+
