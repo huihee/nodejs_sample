93679fa0d2417f3da5cef65c3fed6fa27a34dc34
CHJ <huihee@g.hongik.ac.kr>
2025-01-09 16:14:58 +0900

    특수 기능 로봇 클래스를 추가하는 경우(상속 전)

--- src/class/class.ts
+++ src/class/class.ts
@@ -91,3 +91,72 @@ console.log(user2.username); // cnffur : 김철수
 // 3. 메서드 호출
 user1.study();
 user2.study();
+
+// 클래스의 상속
+class CleaningRobot { // Members
+    // Property(속성) 또는 Field(속성, 필드)
+    name: string;
+    model: string;
+    cleaningSchedule: string[];
+    status: string = "Active"; // 기본값 Default 값을 설정 할 수 있음
+
+    // Constructor(생성자) / 값의 초기화와 관련있기 때문에 필수 요소이다.
+    // 객체의 초기상태를 설정하기 위해 사용
+    // 객체의 속성값을 쉽게 전달 가능
+    constructor(name: string, model: string, cleaningSchedule: string[]){ // param, 매개변수 (전달받는 값)
+        // this의 name과 parma의 name은 다르다
+        // this.name은 클래스의 name, this = Robot
+        this.name = name; // 매개변수로 필드 초기화
+        this.model = model;
+        this.cleaningSchedule = cleaningSchedule;
+    } // 객체가 만들어진 후
+
+    // Method(행동)
+    // 클래스를 사용하면 한번에 수정 가능
+    performTask(task: string){
+        console.log(`${this.name} is performing ${task}.`)
+    };
+
+    updateStatus(newStatus: string) {
+        this.status = newStatus;
+        console.log(`${this.name}'s status is now ${this.status}`);
+    };
+
+    performCleaning() {
+        console.log(`${this.name} is cleaning according to the schedule ${this.cleaningSchedule.join(", ")}`);
+    }
+}
+
+class CookingRobot { // Members
+    // Property(속성) 또는 Field(속성, 필드)
+    name: string;
+    model: string;
+    availableMenus: string[];
+    status: string = "Active"; // 기본값 Default 값을 설정 할 수 있음
+
+    // Constructor(생성자) / 값의 초기화와 관련있기 때문에 필수 요소이다.
+    // 객체의 초기상태를 설정하기 위해 사용
+    // 객체의 속성값을 쉽게 전달 가능
+    constructor(name: string, model: string, availableMenus: string[]){ // param, 매개변수 (전달받는 값)
+        // this의 name과 parma의 name은 다르다
+        // this.name은 클래스의 name, this = Robot
+        this.name = name; // 매개변수로 필드 초기화
+        this.model = model;
+        this.availableMenus = availableMenus;
+    } // 객체가 만들어진 후
+
+    // Method(행동)
+    // 클래스를 사용하면 한번에 수정 가능
+    performTask(task: string){
+        console.log(`${this.name} is performing ${task}.`)
+    };
+
+    updateStatus(newStatus: string) {
+        this.status = newStatus;
+        console.log(`${this.name}'s status is now ${this.status}`);
+    };
+
+    performCleaning() {
+        console.log(`${this.name} is cooking according to the menus ${this.availableMenus.join(", ")}.`);
+    }
+}
